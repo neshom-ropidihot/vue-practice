ecb67482cb7fad318d1d6bc1aee5759407c7aad6
Mohsen Tohidipour <tohidipor@gmail.com>
2023-07-29 14:57:03 +0330

    add 2.ListRendering - learn

--- learn/v2.vuejs.org/assets/index.js
+++ learn/v2.vuejs.org/assets/index.js
@@ -70,3 +70,117 @@ var app7 = new Vue({
     ],
   },
 });
+
+
+// 2.List Rendering
+
+var example1 = new Vue({
+  el: "#example-1",
+  data: {
+    items: [{ message: "foo" }, { message: "bar" }],
+  },
+});
+
+var example2 = new Vue({
+  el : '#example-2',
+  data : {
+    parentMessage : 'parent',
+    items : [
+      { message : 'foo'},
+      { message : 'bar'}
+    ]
+  }
+});
+
+var vforobject = new Vue({
+  el: "#v-for-object",
+  data: {
+    object: {
+      title: "How to do lists in Vue",
+      author: "Jane Doe",
+      publishedAt: "2016-04-10",
+    },
+  },
+});
+
+var evenNumber = new Vue({
+  el: "#even-number",
+  data: {
+    numbers: [1, 2, 3, 4, 5],
+  },
+  computed: {
+    evenNumbers : function() {
+      return this.numbers.filter(function(num) {
+        return num % 2 === 0
+      })
+    }
+  },
+});
+
+var evenNumber2 = new Vue({
+  el: "#even-number-2",
+  data : {
+    sets: [
+      [1,2,3,4,5],
+      [6,7,8,9,10],
+    ],
+  },
+  methods : {
+    even : function(number) {
+      return number.filter(function(n) {
+        return n % 2 === 0
+      });
+    }
+  }
+});
+
+var range = new Vue ({
+  el : '#range',
+  
+});
+
+
+Vue.component("todo-item", {
+  template:
+    "\
+    <li>\
+      {{ title }}\
+      <button v-on:click=\"$emit('remove')\">Remove</button>\
+    </li>\
+  ",
+  props: ["title"],
+});
+
+// ;
+var todolistexample = new Vue({
+  el: "#todo-list-example",
+  data: {
+    newTodoText: "",
+    todos: [
+      {
+        id: 1,
+        title: "Do the dishes",
+      },
+      {
+        id: 2,
+        title: "Take out the trash",
+      },
+      {
+        id: 3,
+        title: "Mow the lawn",
+      },
+    ],
+    nextTodoId: 4,
+  },
+  methods: {
+    addNewTodo: function () {
+
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText,
+      });
+      this.newTodoText = "";
+      
+    },
+  },
+});
