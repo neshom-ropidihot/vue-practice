5e72704a4e473f6e8cc22c776f1ab61954ea4e4c
Mohsen Tohidipour <tohidipor@gmail.com>
2023-07-29 15:19:40 +0330

    add 4.ListRendering - practice

--- learn/assets/main.js
+++ learn/assets/main.js
@@ -5,5 +5,17 @@ var app = new Vue({
     image: "assets/one.jpg",
     inStock: true,
     inventory: 100,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+      },
+    ],
+    sizes: ["S", "M", "L", "XL", "XXL", "XXXL"],
   },
 });
