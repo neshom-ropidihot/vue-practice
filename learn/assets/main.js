d40113a71165e86c5fa472466a80207029e665d8
Mohsen Tohidipour <tohidipor@gmail.com>
2023-08-09 09:01:09 +0330

    7. Computed Properties - learn - vuemastery

--- learn/assets/main.js
+++ learn/assets/main.js
@@ -1,32 +1,47 @@
 var app = new Vue({
   el: "#app",
   data: {
+    brand: "Vue Mastery",
     product: "socks",
-    image: "assets/green.jpg",
-    inStock: true,
+    // image: "assets/green.jpg",
+    selectedVariants: 0,
+    // inStock: true,
     inventory: 100,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
-        variantImage : 'assets/green.jpg',
+        variantImage: "assets/green.jpg",
+        variantQuantity: 10     
       },
       {
         variantId: 2235,
         variantColor: "blue",
-        variantImage : 'assets/blue.jpg',
+        variantImage: "assets/blue.jpg",
+        variantQuantity: 0
       },
     ],
     sizes: ["S", "M", "L", "XL", "XXL", "XXXL"],
-    cart : 0,
+    cart: 0,
   },
-  methods : {
-    addToCart : function() {
-      this.cart += 1; 
+  methods: {
+    addToCart: function () {
+      this.cart += 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariants = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariants].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariants].variantQuantity;
     }
-  }
+  },
 });
