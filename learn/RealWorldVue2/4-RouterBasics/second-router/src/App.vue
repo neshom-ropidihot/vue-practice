8259ec0cc30755b73b76237aa07091a91b4cfce2
Mohsen Tohidipour <tohidipor@gmail.com>
2023-12-11 08:46:41 +0330

    add second-router project

---
+++ learn/RealWorldVue2/4-RouterBasics/second-router/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-link :to="{ name: 'event-list'}"> list</router-link>
+    <router-link :to="{ name: 'event-show'}"> show </router-link>
+    <router-link :to="{ name: 'event-create'}"> create</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ learn/RealWorldVue2/4-RouterBasics/second-router/src/router.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import EventList from '@/views/EventList.vue';
+import EventShow from '@/views/EventShow.vue';
+import EventCreate from '@/views/EventCreate.vue';
+
+Vue.use(Router);
+
+export default new Router ({
+    routes: [
+        {
+            path: '/',
+            name: 'event-list',
+            component: EventList,
+        },
+        {
+            path: '/event',
+            name: 'event-show',
+            component: EventShow,
+        },
+        {
+            path: '/event/create',
+            name: 'event-create',
+            component: EventCreate,
+        },
+    ]
+});
